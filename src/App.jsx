1b626d3191fa46ae54d4985313af0f0491e4034a
Parmjeet <parmjeetmishra2@gmail.com>
2023-12-25 18:28:18 +0530

    react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2>Amarjeet Mishra's Portfolio</h2>
+      <p>Portfolio is comming soon....</p>
+    </>
+  )
+}
+
+export default App
