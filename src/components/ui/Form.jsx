c908825bdf63cd034113e3714ccf5a8d6fddd1c2
Parmjeet <iamparmjeetmishra@gmail.com>
2024-01-08 19:37:19 +0530

    Update: Form lable

--- src/components/ui/Form.jsx
+++ src/components/ui/Form.jsx
@@ -31,23 +31,23 @@ export default function Form() {
         <>
             <form onSubmit={handleSubmit(submitData)} className='w-full mx-auto'>
                 <div className='mb-5 flex flex-col gap-2'>
-                    <label className="block text-base font-medium text-gray-900 "> Full Name:</label>
-                    <input type="text" {...register('fullName')} placeholder='Your Good Name' className="p-2 rounded-lg" />
+                    <label htmlFor='fullName' className="block text-base font-medium text-gray-900 "> Full Name:</label>
+                    <input id='fullName' type="text" {...register('fullName')} placeholder='Your Good Name' className="p-2 rounded-lg" />
                     {errors.fullName && <div className='text-red-600'>{errors.fullName.message}</div>}
                 </div>
                 <div className='mb-5 flex flex-col gap-2'>
-                    <label className="block text-base font-medium text-gray-900 "> Email:</label>
-                    <input placeholder='Your Email' type="email" {...register('email')}  className="p-2 rounded-lg" />
+                    <label htmlFor='email' className="block text-base font-medium text-gray-900 "> Email:</label>
+                    <input id='email' placeholder='Your Email' type="email" {...register('email')}  className="p-2 rounded-lg" />
                     {errors.email && <span className='text-red-600'>{errors.email.message}</span>}
                 </div>
                 <div className='mb-5 flex flex-col gap-2'>
-                    <label className="block text-base font-medium text-gray-900 "> Subject</label>
-                    <input type="string" {...register('subject')} placeholder='Subject'  className="p-2 rounded-lg" />
+                    <label htmlFor='subject' className="block text-base font-medium text-gray-900 "> Subject</label>
+                    <input id='subject' type="string" {...register('subject')} placeholder='Subject'  className="p-2 rounded-lg" />
                     {errors.subject && <span className='text-red-600'>{errors.subject.message}</span>}
                 </div>
                 <div className='mb-5 flex flex-col gap-2'>
-                    <label className="block text-base font-medium text-gray-900 "> Message</label>
-                    <textarea type="text" {...register('message')} placeholder='Your Message'  className="p-2 rounded-lg" />
+                    <label htmlFor='message' className="block text-base font-medium text-gray-900 "> Message</label>
+                    <textarea id='message' type="text" {...register('message')} placeholder='Your Message'  className="p-2 rounded-lg" />
                     {errors.message && <span className='text-red-600'>{errors.message.message}</span>}
                 </div>
                 <input type='submit' className='border-2 border-black  px-2.5 py-3 rounded-xl font-bold text-black hover:bg-cyan-100 hover:text-black hover:border-black transition-all w-full active:border-cyan-200 '  />
